3fb750c6ee1ff73088d0e4eeb0b68df2ab1f8e23
manav <manavkhadka2004@gmail.com>
2024-04-09 23:52:59 +0530

    interceptor modified and public conctroller added

--- src/main/java/com/example/DockApiBackend/config/AuthorizationInterceptor.java
+++ src/main/java/com/example/DockApiBackend/config/AuthorizationInterceptor.java
@@ -3,6 +3,7 @@ package com.example.DockApiBackend.config;// AuthorizationInterceptor.java
 import com.example.DockApiBackend.Service.GitHubApiService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 @Component
+@Log
 public class AuthorizationInterceptor implements HandlerInterceptor {
 
     @Autowired
@@ -18,7 +20,9 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
-        if( request.getRequestURI().contains("/public/")) {
+        log.info("Token: " + token);
+        log.info("Request URI: " + request.getRequestURI());
+        if( request.getRequestURI().contains("/public")) {
             return true;
         }
         if (token != null && gitHubApiService.isValidGitHubToken(token)) {
